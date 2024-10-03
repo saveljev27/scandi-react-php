6bf9f6590c81bb07b284c284e06531720c984d42
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-10-03 23:35:12 +0300

    product-list problem 16

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,15 +3,38 @@
 import Link from 'next/link';
 import { useProducts } from './hooks/useProducts';
 import { ProductCard } from './components';
-import Main from './components/main';
 
 export default function Home() {
   const { products, handleCheckBoxChange, handleDelete } = useProducts();
 
   return (
-    <>
-      <h1>Product List</h1>
-      <Main />
-    </>
+    <form id="product_form" className="container" onSubmit={handleDelete}>
+      <div className="header">
+        <h1>Product List</h1>
+        <div className="header__buttons">
+          <Link href="/add-product">
+            <button type="button" className="btn btn-secondary">
+              ADD
+            </button>
+          </Link>
+          <button
+            id="delete-product-btn"
+            type="submit"
+            className="btn btn-danger"
+          >
+            MASS DELETE
+          </button>
+        </div>
+      </div>
+      <div className="content product-list">
+        {products.map((product) => (
+          <ProductCard
+            key={product.sku}
+            product={product}
+            onChange={handleCheckBoxChange}
+          />
+        ))}
+      </div>
+    </form>
   );
 }
